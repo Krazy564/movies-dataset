744fff7a02e9e6f09e7be45f51811d56976ba6a3
Krazy564 <k1holy@hotmail.com>
2026-03-14 22:01:10 +0000

    git add . git commit git push

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,16 +7,16 @@ fil = "data/budget_fil.xlsx"
 
 def main_st(fil):
 
-    st.header("Budget app", divider="red")
-
-
     df = pd.read_excel(fil, index_col=0)
 
     kolonner = df.columns[1:-1]
 
     df["Total"] = df[kolonner].sum(axis=1)
 
-    st.session_state.poster = st.data_editor(df, num_rows="dynamic")
+    st.subheader("Udgifter", divider="red")
+
+    with st.expander("Budget tabel"):
+        st.session_state.poster = st.data_editor(df, num_rows="dynamic")
 
     total_måneder = df.columns[1:]
 
@@ -24,28 +24,67 @@ def main_st(fil):
 
     df_totaler = pd.DataFrame([df_totaler], index=["Total pr. måned"])
 
-    st.subheader("Total pr. måned", divider="red")
+    #st.subheader("Total pr. måned", divider="red")
     st.dataframe(df_totaler)
+
+    st.subheader("Indtægter", divider="red")
+    indtægter = pd.read_excel("data/indtægter_fil.xlsx", index_col=0)
+    indtægter["Total"] = indtægter[indtægter.columns[0:-1]].sum(axis=1)
+    
+    st.session_state.indtægter = st.data_editor(indtægter)
+
+    st.subheader("Difference", divider="red")
+    
+    
+    out_values = indtægter.to_numpy() - df_totaler.to_numpy()
+    diff = pd.DataFrame(out_values, index=["Difference"], columns=df_totaler.columns)
+    
+
+    st.dataframe(diff)
 
     if st.button("Gem ændringer"):
         st.session_state.poster.to_excel("data/budget_fil.xlsx")
+        st.session_state.indtægter.to_excel("data/indtægter_fil.xlsx")
         st.rerun()
     
     
+    col1, col2 = st.columns([1,1])
+
+    df_totaler["Indtægt"] = indtægter["Total"]
+
+    
+
     plot_df = (
         df_totaler.T
+        .reset_index()
+        .rename(columns={"index": "Måned", 0: "Total", 0:"Indtægt"})
+    )
+    
+    plot_df.loc[13] = {
+        "Måned":"Indtægt",
+        "Total pr. måned":indtægter["Total"]
+    }
+
+
+    myplot = px.bar(plot_df, x="Måned", y="Total pr. måned", color="Måned", text_auto="True", title="Udgifter")
+
+    plot_df_diff = (
+        diff.T
         .reset_index()
         .rename(columns={"index": "Måned", 0: "Total"})
     )
 
-    myplot = px.bar(plot_df, x="Måned", y="Total pr. måned", color="Måned", text_auto="True")
 
-    st.plotly_chart(myplot)
+    myplot_diff = px.bar(plot_df_diff, x="Måned", y="Difference", color="Måned", text_auto="True", title="Difference")
+
+    col1.plotly_chart(myplot)
+    col2.plotly_chart(myplot_diff)
+
 
 
 def login_gate():
     st.set_page_config(layout="wide")
-    st.title("Login")
+    st.title("Budget app")
 
     # Session init
     if "authenticated" not in st.session_state:
